3cf2842cefa397fddd1f2c7e4bc35f39855469fa
jessworks <48979913+jessworks@users.noreply.github.com>
2023-11-26 13:38:23 +0100

    feat: todo and sandbox added, pseudo arithmetics

---
+++ script.js
@@ -0,0 +1,20 @@
+const donuts = [
+    {
+        name: 'cherry',
+        price: 45,
+        rating: 5,
+        category: 'special agent',
+    },
+    {
+        name: 'apple',
+        price: 42,
+        rating: 3.5,
+        category: 'state trooper',
+    },
+    {
+        name: 'coffee',
+        price: 38,
+        rating: 4,
+        category: 'special agent',
+    }
+]
