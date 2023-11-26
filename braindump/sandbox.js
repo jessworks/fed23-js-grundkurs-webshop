04d1187f0fe5050f8de5a64ecb9a4dc120b8d46e
jessworks <48979913+jessworks@users.noreply.github.com>
2023-11-26 15:17:13 +0100

    feat: product array, order form & buttons in html

--- braindump/sandbox.js
+++ braindump/sandbox.js
@@ -7,20 +7,20 @@ if och definiera tidsram med hjälp av dagar och klockslag
 ! kom ihåg parenteserna, det är matte
 ex mimmi o musse pigg
 
-! koppla den till det specifika elementet
+! koppla den till det specifika elementet, fundera på placering
     -må 03-10 -10% på order form sumTotal
         -if... dag && (större än 03 || mindre än 10) {order sumTotal * 0.9;
         påa rabatten --> skapa element separat och anropa här för cleaner if}
-        -printa på nytt, anropa i vad?
+        -printa på nytt, anropa produktloop med prisjustering som tillägg?
     -fr 15- må 03 +15% på price
         -if... (större än dag och tid) || mindre än dag och  tid) {prishöjning på produktsida price * 1.15}
-        -printa price på nytt, anropa i produktloop?
+        -printa price på nytt, produktloop?
     -10+ samma produkt -10%
         -if... produktinputrutan? > 10...{rabatt på enskilda produktens totalsumma * 0.9}
-        -printa produktens totalsumma på nytt, anropa i produktloop?
+        -printa produktens totalsumma på nytt, produktloop?
     -16+ products fri lev.
         -if... productsTotal > 16... {disable deliveryCost-beräkningen}
-        -anropa i deliveryCost-beräkningen?
+        -i deliveryCost-beräkningen?
     -lev 
         -( 25 + totalPriceProducts * 1.1
     -faktura disabled
@@ -28,7 +28,9 @@ ex mimmi o musse pigg
             -disable faktura alternativet --> hur?
         -på att det inte går där faktura alternativet finns
 
-    -15 min  timeout
+    -15 min  timeout på added to cart --> order button
         -koppla till produktsida och order form, bordew återställa allt. men filter då?
+    -2 sec timeout visual que: added to cart
+        -grön bock/cart växer när produkt lagts till
 
 */
--- script.js
+++ script.js
@@ -1,20 +1,62 @@
-const donuts = [
+const products = [
     {
-        name: 'cherry',
-        price: 45,
+        name: 'Cherry Pie',
+        price: 67,
         rating: 5,
         category: 'special agent',
     },
     {
-        name: 'apple',
-        price: 42,
-        rating: 3.5,
+        name: 'Apple Pie',
+        price: 67,
+        rating: 4,
         category: 'state trooper',
     },
     {
-        name: 'coffee',
-        price: 38,
+        name: 'Flat White',
+        price: 47,
+        rating: 4,
+        category: 'state trooper',
+    },
+    {
+        name: 'Cofee, Black',
+        price: 42,
         rating: 4,
         category: 'special agent',
+    },
+    {
+        name: 'Cherry Glazed Donut',
+        price: 54,
+        rating: 3,
+        category: 'log lady',
+    },
+    {
+        name: 'Chocolate Glazed Donut',
+        price: 54,
+        rating: 4,
+        category: 'special agent',
+    },
+    {
+        name: 'Donut, plain',
+        price: 49,
+        rating: 3,
+        category: 'special agent',
+    },
+    {
+        name: 'Truffle Chip Cookie',
+        price: 47,
+        rating: 4.5,
+        category: 'log lady',
+    },
+    {
+        name: 'Ham on Rye',
+        price: 67,
+        rating: 4,
+        category: 'special agent',
+    },
+    {
+        name: 'Grilled Cheese',
+        price: 59,
+        rating: 3.5,
+        category: 'state trooper',
     }
 ]
