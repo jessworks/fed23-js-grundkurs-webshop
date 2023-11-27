46cf3d1e175946f0e247be356ed215fddfec1025
jessworks <48979913+jessworks@users.noreply.github.com>
2023-11-27 14:27:48 +0100

    feat: inc and dec numbeer of donuts in cart

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const products = [
         price: 67,
         rating: 5,
         category: 'special agent',
+        amount: 0,
     },
     {
         image: [
@@ -24,6 +25,7 @@ const products = [
         price: 67,
         rating: 4,
         category: 'state trooper',
+        amount: 0,
     },
     {
         image: [
@@ -36,6 +38,7 @@ const products = [
         price: 47,
         rating: 4,
         category: 'state trooper',
+        amount: 0,
     },
     {
         image: [
@@ -48,6 +51,7 @@ const products = [
         price: 42,
         rating: 4,
         category: 'special agent',
+        amount: 0,
     },
     {
         image: [
@@ -60,6 +64,7 @@ const products = [
         price: 54,
         rating: 3,
         category: 'log lady',
+        amount: 0,
     },
     {
         image: [
@@ -72,6 +77,7 @@ const products = [
         price: 54,
         rating: 4,
         category: 'special agent',
+        amount: 0,
     },
     {
         image: [
@@ -84,6 +90,7 @@ const products = [
         price: 49,
         rating: 3,
         category: 'special agent',
+        amount: 0,
     },
     {
         image: [
@@ -96,6 +103,7 @@ const products = [
         price: 47,
         rating: 4.5,
         category: 'log lady',
+        amount: 0,
     },
     {
         image: [
@@ -108,6 +116,7 @@ const products = [
         price: 67,
         rating: 4,
         category: 'special agent',
+        amount: 0,
     },
     {
         image: [
@@ -120,13 +129,30 @@ const products = [
         price: 59,
         rating: 3.5,
         category: 'state trooper',
+        amount: 0,
     }
-]
+];
+
+function decreaseAmount(e) {
+    const index = e.currentTarget.dataset.id;
+    if (products[index].amount >= 0) {
+        products[index].amount = 0;
+    } else {
+        products[index].amount -= 1;
+    }
+    printProducts();
+};
+
+function increaseAmount(e) {
+    const index = e.currentTarget.dataset.id;
+    products[index].amount += 1;
+    printProducts();
+};
 
 function printProducts() {
     productsContainerHtml.innerHTML = '';
 
-    products.forEach(products => {
+    products.forEach((products, index) => {
         productsContainerHtml.innerHTML += 
         `
             <li>
@@ -135,11 +161,23 @@ function printProducts() {
                 <div>Price: <span>${products.price}</span> kr</div>
                 <div>Rating: <span>${products.rating}</span></div>
                 <div>Category: <span>${products.category}</span></div>
-                <button>-</button>
-                <button>+</button>
+                <button class="decrease" data-id="${index}">-</button>
+                <div>Amount: <span>${products.amount}</span></div>
+                <button class="increase" data-id="${index}">+</button>
             </li>
         `;
     });
-}
+
+    const btnDecrease = document.querySelectorAll('button.decrease');
+    const btnIncrease = document.querySelectorAll('button.increase');
+
+    btnDecrease.forEach(btn => {
+        btn.addEventListener('click', decreaseAmount);
+    });
+
+    btnIncrease.forEach(btn => {
+        btn.addEventListener('click', increaseAmount);
+    });
+};
 
 printProducts();
