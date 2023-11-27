b4c677d892226be0f4adfbf3cffb9a4d6cd4e83b
jessworks <48979913+jessworks@users.noreply.github.com>
2023-11-27 13:19:58 +0100

    feat: products printed

--- script.js
+++ script.js
@@ -1,62 +1,145 @@
+const productsContainerHtml = document.querySelector('#productsContainer');
+
 const products = [
     {
+        image: [
+            {
+                src: 'images/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg',
+                alt: 'placeholder',
+            },
+        ],
         name: 'Cherry Pie',
         price: 67,
         rating: 5,
         category: 'special agent',
     },
     {
+        image: [
+            {
+                src: 'images/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg',
+                alt: 'placeholder',
+            },
+        ],
         name: 'Apple Pie',
         price: 67,
         rating: 4,
         category: 'state trooper',
     },
     {
+        image: [
+            {
+                src: 'images/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg',
+                alt: 'placeholder',
+            },
+        ],
         name: 'Flat White',
         price: 47,
         rating: 4,
         category: 'state trooper',
     },
     {
+        image: [
+            {
+                src: 'images/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg',
+                alt: 'placeholder',
+            },
+        ],
         name: 'Cofee, Black',
         price: 42,
         rating: 4,
         category: 'special agent',
     },
     {
+        image: [
+        {
+            src: 'images/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg',
+            alt: 'placeholder',
+        },
+    ],
         name: 'Cherry Glazed Donut',
         price: 54,
         rating: 3,
         category: 'log lady',
     },
     {
+        image: [
+            {
+                src: 'images/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg',
+                alt: 'placeholder',
+            },
+        ],
         name: 'Chocolate Glazed Donut',
         price: 54,
         rating: 4,
         category: 'special agent',
     },
     {
+        image: [
+            {
+                src: 'images/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg',
+                alt: 'placeholder',
+            },
+        ],
         name: 'Donut, plain',
         price: 49,
         rating: 3,
         category: 'special agent',
     },
     {
+        image: [
+            {
+                src: 'images/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg',
+                alt: 'placeholder',
+            },
+        ],
         name: 'Truffle Chip Cookie',
         price: 47,
         rating: 4.5,
         category: 'log lady',
     },
     {
+        image: [
+            {
+                src: 'images/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg',
+                alt: 'placeholder',
+            },
+        ],
         name: 'Ham on Rye',
         price: 67,
         rating: 4,
         category: 'special agent',
     },
     {
+        image: [
+            {
+                src: 'images/images/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg',
+                alt: 'placeholder',
+            },
+        ],
         name: 'Grilled Cheese',
         price: 59,
         rating: 3.5,
         category: 'state trooper',
     }
 ]
+
+function printProducts() {
+    productsContainerHtml.innerHTML = '';
+
+    products.forEach(products => {
+        productsContainerHtml.innerHTML += 
+        `
+            <li>
+                <img>${products.image}
+                <h2>${products.name}</h2>
+                <div>Price: <span>${products.price}</span> kr</div>
+                <div>Rating: <span>${products.rating}</span></div>
+                <div>Category: <span>${products.category}</span></div>
+                <button>-</button>
+                <button>+</button>
+            </li>
+        `;
+    });
+}
+
+printProducts();
